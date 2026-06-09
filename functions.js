9cd9dfdae44359abb69c42e264842c7c02eb3412
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-06-09 18:34:26 +0530

    Node Pilot

---
+++ functions.js
@@ -0,0 +1,23 @@
+export default function evenOdd(x) {
+    if (x % 2 == 0) {
+        console.log("even");
+    }
+    else {
+        console.log("Odd");
+    }
+}
+export function add(x,y) {
+    console.log(x+y);    
+}
+export function sub(x,y) {
+    console.log(x-y);    
+}
+export function mul(x,y) {
+    console.log(x*y);    
+}
+export function div(x,y) {
+    console.log(x/y);    
+}
+export function mod(x,y) {
+    console.log(x%y);    
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+import evenOdd ,{add,sub,mul,div,mod}from './functions.js'
+
+var a = 24;
+var b = 30;
+var c = 55;
+
+evenOdd(4)
+add(456,75)
